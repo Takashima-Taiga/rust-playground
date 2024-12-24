31619bdf003377380b8f500270b866b5ec6f26bb
Takashima-Taiga <t.taiga815@gmail.com>
2024-12-25 00:41:42 +0900

    create dummy of echo cmd

--- app/echor/src/main.rs
+++ app/echor/src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version = "1.0.0", about, long_about = None)]
+struct Args {
+    #[arg(required = true)]
+    input: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{}", args.input.join(" "));
 }
